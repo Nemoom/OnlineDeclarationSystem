84a0d4280e53be1bf5a0251d5132a40fef994eb5
Tobby Li <tobby.li@festo.com>
2020-04-10 09:27:34 +0800

    change to chinese

--- OnlineDeclarationSystem/Form1.Designer.cs
+++ OnlineDeclarationSystem/Form1.Designer.cs
@@ -31,21 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.rBtn_pm = new System.Windows.Forms.RadioButton();
-            this.rBtn_am = new System.Windows.Forms.RadioButton();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rBtn_pm = new System.Windows.Forms.RadioButton();
+            this.rBtn_am = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -59,7 +59,7 @@
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(623, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(636, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(164, 417);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -69,8 +69,8 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.91653F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.08347F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.99351F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.00649F));
             this.tableLayoutPanel1.Controls.Add(this.comboBox3, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
@@ -93,58 +93,101 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 84F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(623, 417);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(636, 417);
             this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(140, 147);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(241, 39);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(140, 85);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(241, 39);
+            this.comboBox2.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(23, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 31);
+            this.label1.Size = new System.Drawing.Size(78, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Name:";
+            this.label1.Text = "姓名:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(23, 82);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 31);
+            this.label2.Size = new System.Drawing.Size(106, 31);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Sub-Dept:";
+            this.label2.Text = "子行业:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(23, 144);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 31);
+            this.label3.Size = new System.Drawing.Size(78, 31);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Type:";
+            this.label3.Text = "用途:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(23, 206);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 31);
+            this.label4.Size = new System.Drawing.Size(78, 31);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Date:";
+            this.label4.Text = "时间:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(23, 268);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 31);
+            this.label5.Size = new System.Drawing.Size(78, 31);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Desc:";
+            this.label5.Text = "描述:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(241, 39);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 353);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 23, 3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 48);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(140, 271);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(493, 56);
+            this.textBox1.TabIndex = 13;
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(167, 209);
+            this.splitContainer1.Location = new System.Drawing.Point(140, 209);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -154,40 +197,16 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.panel1);
-            this.splitContainer1.Size = new System.Drawing.Size(453, 56);
-            this.splitContainer1.SplitterDistance = 263;
+            this.splitContainer1.Size = new System.Drawing.Size(493, 56);
+            this.splitContainer1.SplitterDistance = 286;
             this.splitContainer1.TabIndex = 5;
-            // 
-            // rBtn_pm
-            // 
-            this.rBtn_pm.AutoSize = true;
-            this.rBtn_pm.Dock = System.Windows.Forms.DockStyle.Left;
-            this.rBtn_pm.Location = new System.Drawing.Point(94, 0);
-            this.rBtn_pm.Name = "rBtn_pm";
-            this.rBtn_pm.Size = new System.Drawing.Size(85, 38);
-            this.rBtn_pm.TabIndex = 0;
-            this.rBtn_pm.Text = "p.m.";
-            this.rBtn_pm.UseVisualStyleBackColor = true;
-            // 
-            // rBtn_am
-            // 
-            this.rBtn_am.Checked = true;
-            this.rBtn_am.Dock = System.Windows.Forms.DockStyle.Left;
-            this.rBtn_am.Location = new System.Drawing.Point(0, 0);
-            this.rBtn_am.Name = "rBtn_am";
-            this.rBtn_am.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.rBtn_am.Size = new System.Drawing.Size(94, 38);
-            this.rBtn_am.TabIndex = 1;
-            this.rBtn_am.TabStop = true;
-            this.rBtn_am.Text = "a.m.";
-            this.rBtn_am.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dateTimePicker1.Location = new System.Drawing.Point(0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(263, 38);
+            this.dateTimePicker1.Size = new System.Drawing.Size(286, 38);
             this.dateTimePicker1.TabIndex = 0;
             this.dateTimePicker1.Value = new System.DateTime(2020, 4, 7, 0, 0, 0, 0);
             // 
@@ -198,57 +217,38 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(186, 38);
+            this.panel1.Size = new System.Drawing.Size(203, 38);
             this.panel1.TabIndex = 2;
             // 
-            // comboBox1
+            // rBtn_pm
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(167, 23);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(241, 39);
-            this.comboBox1.TabIndex = 7;
+            this.rBtn_pm.AutoSize = true;
+            this.rBtn_pm.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rBtn_pm.Location = new System.Drawing.Point(107, 0);
+            this.rBtn_pm.Name = "rBtn_pm";
+            this.rBtn_pm.Size = new System.Drawing.Size(91, 38);
+            this.rBtn_pm.TabIndex = 0;
+            this.rBtn_pm.Text = "下午";
+            this.rBtn_pm.UseVisualStyleBackColor = true;
             // 
-            // comboBox2
+            // rBtn_am
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(167, 85);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(241, 39);
-            this.comboBox2.TabIndex = 8;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(167, 353);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 23, 3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(255, 48);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(167, 147);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(241, 39);
-            this.comboBox3.TabIndex = 12;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(167, 271);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(453, 56);
-            this.textBox1.TabIndex = 13;
+            this.rBtn_am.Checked = true;
+            this.rBtn_am.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rBtn_am.Location = new System.Drawing.Point(0, 0);
+            this.rBtn_am.Name = "rBtn_am";
+            this.rBtn_am.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.rBtn_am.Size = new System.Drawing.Size(107, 38);
+            this.rBtn_am.TabIndex = 1;
+            this.rBtn_am.TabStop = true;
+            this.rBtn_am.Text = "上午";
+            this.rBtn_am.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 417);
+            this.ClientSize = new System.Drawing.Size(800, 417);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("MetaPlusLF", 16F);
